dbe4cf217f0eb2a5361dba12e287773bfb951e1a
maplestoria <zhongkx09@outlook.com>
2025-12-13 14:46:33 +0800

    Refactor user blocking logic to handle IDs and nicknames from JSON response

--- tampermonkey-purifyjandan.js
+++ tampermonkey-purifyjandan.js
@@ -20,17 +20,23 @@
     GM_log("Purify Jandan: Last update time:", new Date(lastUpdateTime).toLocaleString());
 
     let blockedNickNames = localStorage.getItem("jandan:blockNickStore");
-    let jso = blockedNickNames ? JSON.parse(blockedNickNames) : { blockedUsers: {} };
-    if (!jso.blockedUsers || typeof jso.blockedUsers !== 'object') {
-        jso.blockedUsers = {};
+    let blockNickStore = blockedNickNames ? JSON.parse(blockedNickNames) : { blockedUsers: {} };
+    if (!blockNickStore.blockedUsers || typeof blockNickStore.blockedUsers !== 'object') {
+        blockNickStore.blockedUsers = {};
+    }
+
+    let blockedIds = localStorage.getItem("jandan:blockIDStore");
+    let blockIDStore = blockedIds ? JSON.parse(blockedIds) : { blockedUsers: {} };
+    if (!blockIDStore.blockedUsers || typeof blockIDStore.blockedUsers !== 'object') {
+        blockIDStore.blockedUsers = {};
     }
 
     if (!lastUpdateTime
         || (Date.now() - lastUpdateTime) > updateInterval
-        || Object.keys(jso.blockedUsers).length === 0) {
+        || Object.keys(blockNickStore.blockedUsers).length === 0) {
         GM_log("Purify Jandan: Fetching updated blocked users list...");
 
-        const blockedUsers = "https://raw.githubusercontent.com/maplestoria/PurifyJandan/refs/heads/main/blocked_users.txt";
+        const blockedUsers = "https://raw.githubusercontent.com/maplestoria/PurifyJandan/refs/heads/main/blocked_users.json";
 
         GM_xmlhttpRequest({
             method: "GET",
@@ -38,18 +44,23 @@
             nocache: true,
             timeout: 10000,
             onload: function (resp) {
-                resp.responseText.split("\n")
-                    .map(name => name.trim())
-                    .filter(name => name.length > 0)
-                    .forEach(name => {
-                        if (!jso.blockedUsers[name]) {
-                            GM_log("Purify Jandan: Blocking user:", name);
-                            jso.blockedUsers[name] = true;
-                            localStorage.setItem("jandan:blockNickStore", JSON.stringify(jso));
-                        }
-                    });
+                const blcoked = JSON.parse(resp.responseText);
+                blcoked.nicknames.forEach(name => {
+                    if (!blockNickStore.blockedUsers[name]) {
+                        GM_log("Purify Jandan: Blocking user:", name);
+                        blockNickStore.blockedUsers[name] = true;
+                    }
+                });
+                blcoked.ids.forEach(id => {
+                    if (!blockIDStore.blockedUsers[id]) {
+                        GM_log("Purify Jandan: Blocking user ID:", id);
+                        blockIDStore.blockedUsers[id] = true;
+                    }
+                });
+                localStorage.setItem("jandan:blockIDStore", JSON.stringify(blockIDStore));
+                localStorage.setItem("jandan:blockNickStore", JSON.stringify(blockNickStore));
+
                 GM_setValue(lastUpdateTimeKey, Date.now());
-                localStorage.setItem("jandan:blockNickStore", JSON.stringify(jso));
                 GM_log("Purify Jandan: Blocked users list updated.");
             },
             onerror: function (error) {
